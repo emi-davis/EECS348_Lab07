298d65a49a60204b602a0fd9230f52f15a41de6a
Emilia <EmiliaDavis2027@gmail.com>
2024-11-02 23:56:04 -0500

    sum diagonal m1m2

--- main.cpp
+++ main.cpp
@@ -58,10 +58,17 @@ public:
     }
     return result;
   }
-/*
-  // 5. Compute the sum of matrix diagonal elements
-  int sumOfDiagonals() const;
 
+  // 5. Compute the sum of matrix diagonal elements
+  int sumOfDiagonals() const {
+    int sum = 0;
+    for (int i=0; i < SIZE; i++) {
+      sum += data[i][i];
+      sum += data[i][SIZE-1-i];
+    }
+    return sum;
+  }
+/*
   // 6. Swap matrix rows
   void swapRows(int row1, int row2);*/
 };
@@ -87,9 +94,10 @@ int main() {
   Matrix product = mat1 * mat2;
   cout << "Product of matrices:" << endl;
   product.display();
-/*
-  cout << "Sum of diagonals of Matrix 1: " << mat1.sumOfDiagonals() << endl;
 
+  cout << "Sum of diagonals of Matrix 1: " << mat1.sumOfDiagonals() << endl;
+  cout << "Sum of diagonals of Matrix 2: " << mat2.sumOfDiagonals() << endl;
+/*
   mat1.swapRows(0, 2);
   cout << "Matrix 1 after swapping rows:" << endl;
   mat1.display();*/
